dd02cf6c55deddcd414fdf0376c87da022154763
reinout538 <78550421+reinout538@users.noreply.github.com>
2025-09-29 13:23:46 +0200

    Add files via upload

---
+++ config - example.py
@@ -0,0 +1,10 @@
+#PURE_BASE_URL = 'https://research.vu.nl'
+PURE_BASE_URL = 'https://research.accept.vu.nl'
+
+#PURE_524_API_KEY = ''
+PURE_524_API_KEY = ''
+
+#PURE_CRUD_API_KEY = ''
+PURE_CRUD_API_KEY = ''
+
+SCOPUS_API_KEY = ''
